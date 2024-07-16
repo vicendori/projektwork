ae9c7eb79ac6c80cef40e401b1eec1c6270f2f52
A200119538 <dorina.vicen@telekom.com>
2024-07-16 15:22:23 +0200

    Tesco Search Steps definitions were created, but not working :(

---
+++ src/test/java/TescoSearchSteps.java
@@ -0,0 +1,42 @@
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class TescoSearchSteps {
+    @Given("I open the homepage")
+    public void iOpenTheHomepage() {
+    }
+
+    @And("I accept the cookies")
+    public void iAcceptTheCookies() {
+    }
+
+    @Then("Click into the search field.")
+    public void clickIntoTheSearchField() {
+    }
+
+    @When("I search for {string}")
+    public void iSearchFor(String arg0) {
+    }
+
+    @Then("{int} products are displayed")
+    public void productsAreDisplayed(int arg0) {
+    }
+
+    @And("the product's name contains {string}")
+    public void theProductSNameContains(String arg0) {
+    }
+
+    @Then("No product is displayed")
+    public void noProductIsDisplayed() {
+    }
+
+    @And("{string}message is displayed")
+    public void messageIsDisplayed(String arg0) {
+    }
+
+    @And("the product's names contain the letter k separately")
+    public void theProductSNamesContainTheLetterKSeparately() {
+    }
+}
