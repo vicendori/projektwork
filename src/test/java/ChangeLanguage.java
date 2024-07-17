4d4085cac0cdcd76d6013870a4d56feb43e4ddfb
A200119538 <dorina.vicen@telekom.com>
2024-07-17 09:28:02 +0200

    Language change feature modified.

---
+++ src/test/java/ChangeLanguage.java
@@ -0,0 +1,55 @@
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ChangeLanguage {
+    @Given("that the homepage is open")
+    public void thatTheHomepageIsOpen() {
+        
+    }
+
+    @And("I accept all the cookies")
+    public void iAcceptAllTheCookies() {
+        
+    }
+
+    @Given("the selected language is Hungarian")
+    public void theSelectedLanguageIsHungarian() {
+        
+    }
+
+    @And("the English flag and text is visible")
+    public void theEnglishFlagAndTextIsVisible() {
+        
+    }
+
+    @When("I click on the English flag")
+    public void iClickOnTheEnglishFlag() {
+        
+    }
+
+    @Then("the page language changed to English")
+    public void thePageLanguageChangedToEnglish() {
+        
+    }
+
+    @Given("the selected language is English")
+    public void theSelectedLanguageIsEnglish() {
+        
+    }
+
+    @And("the Hungarian flag and text is visible")
+    public void theHungarianFlagAndTextIsVisible() {
+        
+    }
+
+    @When("I click on the Hungarian flag")
+    public void iClickOnTheHungarianFlag() {
+        
+    }
+
+    @Then("the page language changed to Hungarian")
+    public void thePageLanguageChangedToHungarian() {
+    }
+}
--- src/test/resources/features/languageChange.feature
+++ src/test/resources/features/languageChange.feature
@@ -1,18 +1,18 @@
 Feature: Change the language of the page
 
-  Rule: It is possible to change the language of the page between Hungarian and English
+  Rule: The language of the page between Hungarian and EnglishIt can be changed
 
     Background:
       Given that the homepage is open
       And I accept all the cookies
 
-    Scenario1:  Change the language to English
+    Scenario:  Change the language to English
       Given the selected language is Hungarian
       And the English flag and text is visible
       When I click on the English flag
       Then the page language changed to English
 
-    Scenario2:  Change the language to Hungarian
+    Scenario:  Change the language to Hungarian
       Given the selected language is English
       And the Hungarian flag and text is visible
       When I click on the Hungarian flag
