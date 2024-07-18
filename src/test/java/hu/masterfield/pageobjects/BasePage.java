0663ae17b5a309d0f88783e132f596c249f3552b
A200119538 <dorina.vicen@telekom.com>
2024-07-18 15:21:10 +0200

    Language change feature is working (:probably:)

--- src/test/java/hu/masterfield/pageobjects/BasePage.java
+++ src/test/java/hu/masterfield/pageobjects/BasePage.java
@@ -1,8 +1,31 @@
 package hu.masterfield.pageobjects;
 
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class BasePage {
+
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+//    protected boolean isLoaded(WebElement element){
+//        return wait.withTimeout(Duration.ofSeconds(60)).until(ExpectedConditions.visibilityOf());
+
+    public boolean isLoaded(WebElement element) {
+        try {
+            return wait.until(ExpectedConditions.visibilityOf(element)).isDisplayed();
+        } catch (TimeoutException e) {
+            fail(e);
+            return false;
+        }
+    }
 
     protected WebDriver driverInPageObject;
 
--- src/test/java/hu/masterfield/steps/TescoSteps.java
+++ src/test/java/hu/masterfield/steps/TescoSteps.java
@@ -8,16 +8,16 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 
 public class TescoSteps extends BaseTest {
-
-    private static final String ENGLISH_LANGUAGE = "English";
-    private static final String HUNGARIAN_LANGUAGE = "Hungarian";
-    private static final String LANGUAGE_ELEMENT_ENGLISH_TEXT = "Quick and convenient shopping";
-    private static final String LANGUAGE_ELEMENT_HUNGARIAN_TEXT = "Gyors és kényelmes bevásárlás";
-
 
     @Before
     public void setup () {
@@ -28,6 +28,7 @@ public class TescoSteps extends BaseTest {
     public void tearDown () {
         super.tearDown();
     }
+
 
     @Given("that the homepage is open")
     public void thatTheHomepageIsOpen() {
@@ -45,19 +46,30 @@ public class TescoSteps extends BaseTest {
 
     @Given("the selected language is Hungarian")
     public void theSelectedLanguageIsHungarian() {
+        driver.get("https://bevasarlas.tesco.hu/groceries/hu-HU/");
 
     }
 
 
     @And("the English flag and text is visible")
     public void theEnglishFlagAndTextIsVisible() {
-
+        WebElement englishFlagElement = driver.findElement(By.xpath("//button[@id='utility-header-language-switch-link']"));
+        if (englishFlagElement.isDisplayed()) {
+            System.out.println("The English flag is visible.");
+        }
     }
 
     @When("I change the language to English")
     public void iChangeTheLanguageToEnglish() {
+            WebElement languageChangeElement = driver.findElement(By.xpath("//button[@id='utility-header-language-switch-link']"));
+                if (driver.getCurrentUrl().contains("/groceries/hu-HU/")) {
+                    languageChangeElement.click();
+                }
 
+    }
 
+    private void clickOnHungarianFlag() {
+        driver.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/nav[2]/div/div/div/ul/form")).click();
     }
 
     @Then("the page language changed to English")
@@ -67,16 +79,29 @@ public class TescoSteps extends BaseTest {
 
     @Given("the selected language is English")
     public void theSelectedLanguageIsEnglish() {
+        driver.get("https://bevasarlas.tesco.hu/groceries/en-GB/");
 
     }
 
     @And("the Hungarian flag and text is visible")
     public void theHungarianFlagAndTextIsVisible() {
+        WebElement englishFlagElement = driver.findElement(By.xpath("//button[@id='utility-header-language-switch-link']"));
+        if (englishFlagElement.isDisplayed()) {
+            System.out.println("The Hungarian flag is visible.");
+        }
 
     }
 
     @When("I change the language to Hungarian")
     public void iChangeTheLanguageToHungarian() {
+            WebElement languageChangeElement = driver.findElement(By.xpath("//button[@id='utility-header-language-switch-link']"));
+            if (driver.getCurrentUrl().contains("/groceries/en-GB/")) {
+                languageChangeElement.click();
+            }
+        }
+
+    private void clickOnEnglishFlag() {
+        driver.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/nav[2]/div/div/div/ul/form")).click();
     }
 
 
