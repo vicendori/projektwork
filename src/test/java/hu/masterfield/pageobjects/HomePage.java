4920f9005ab5b311ece762b57297b81839b1ed92
A200119538 <dorina.vicen@telekom.com>
2024-07-19 12:51:29 +0200

    Elements were exported from TescoSteps to HomePage. Language change was tested successfully and expected messages were displayed in the console.

--- src/test/java/hu/masterfield/pageobjects/HomePage.java
+++ src/test/java/hu/masterfield/pageobjects/HomePage.java
@@ -1,37 +1,81 @@
 package hu.masterfield.pageobjects;
 
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-    public class HomePage extends BasePage {
-        WebDriver driver;
 
 
-        @FindBy(xpath = "//button[@class='styled__StyledTextButton-sc-8hxn3m-0 jyQVJq ddsweb-cookies-notification__button ddsweb-button ddsweb-button--text-button']")
-        WebElement cookieNotificationElement;
-        @FindBy(xpath = "//button[@id='utility-header-language-switch-link']")
-        WebElement selectEnglishElement;
+public class HomePage extends BasePage {
+    WebDriver driver;
 
 
-
-        public HomePage(WebDriver driver) {
-            this.driver = driver;
-            PageFactory.initElements(driver, this);
-        }
-
-
-        public void acceptCookies() {
-            if (cookieNotificationElement.isDisplayed()) {
-                cookieNotificationElement.click();
-            }
-        }
-
-        public void isLoaded() {
-        }
+    @FindBy(xpath = "//button[@class='styled__StyledTextButton-sc-8hxn3m-0 jyQVJq ddsweb-cookies-notification__button ddsweb-button ddsweb-button--text-button']")
+    WebElement cookieNotificationElement;
+    @FindBy(xpath = "//button[@id='utility-header-language-switch-link']")
+    WebElement switchLanguageElement;
 
 
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+
+    public void acceptCookies() {
+        driver.findElement(By.xpath("//button[@class='styled__StyledTextButton-sc-8hxn3m-0 jyQVJq ddsweb-cookies-notification__button ddsweb-button ddsweb-button--text-button']")).click();
 
     }
+
+    public void isLoaded() {
+    }
+
+    public void checkEnglishFlag() {
+        WebElement englishFlagElement = driver.findElement(By.xpath("//button[@id='utility-header-language-switch-link']"));
+        if (englishFlagElement.isDisplayed()) {
+            System.out.println("The English flag is visible.");
+        }
+    }
+
+    public void changeLanguageToEng() {
+        WebElement languageChangeElement = driver.findElement(By.xpath("//button[@id='utility-header-language-switch-link']"));
+        if (driver.getCurrentUrl().contains("/groceries/hu-HU/")) {
+            languageChangeElement.click();
+        }
+
+    }
+
+    public void checkLanguageChangedToEng() {
+        if (driver.getCurrentUrl().equals("https://bevasarlas.tesco.hu/groceries/en-GB/")) ;
+        {
+            System.out.println("Language changed to English.");
+        }
+
+    }
+
+    public void checkHungarianFlag() {
+        WebElement englishFlagElement = driver.findElement(By.xpath("//button[@id='utility-header-language-switch-link']"));
+        if (englishFlagElement.isDisplayed()) {
+            System.out.println("The Hungarian flag is visible.");
+        }
+    }
+
+    public void changeLanguageToHun() {
+        WebElement languageChangeElement = driver.findElement(By.xpath("//button[@id='utility-header-language-switch-link']"));
+        if (driver.getCurrentUrl().contains("/groceries/en-GB/")) {
+            languageChangeElement.click();
+        }
+    }
+
+    public void checkLanguageChangedToHun() {
+        if (driver.getCurrentUrl().equals("https://bevasarlas.tesco.hu/groceries/hu-HU/")) ;
+        {
+            System.out.println("Language changed to Hungarian.");
+        }
+
+    }
+}
 
--- src/test/java/hu/masterfield/steps/TescoSteps.java
+++ src/test/java/hu/masterfield/steps/TescoSteps.java
@@ -8,27 +8,23 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
 
+import java.time.Duration;
 
 public class TescoSteps extends BaseTest {
 
     @Before
-    public void setup () {
+    public void setup() {
         super.setup();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     @After
-    public void tearDown () {
+    public void tearDown() {
         super.tearDown();
     }
-
 
     @Given("that the homepage is open")
     public void thatTheHomepageIsOpen() {
@@ -40,8 +36,7 @@ public class TescoSteps extends BaseTest {
 
     @And("I accept all the cookies")
     public void iAcceptAllTheCookies() {
-        driver.findElement(By.xpath("//button[@class='styled__StyledTextButton-sc-8hxn3m-0 jyQVJq ddsweb-cookies-notification__button ddsweb-button ddsweb-button--text-button']")).click();
-
+        homePage.acceptCookies();
     }
 
     @Given("the selected language is Hungarian")
@@ -53,27 +48,17 @@ public class TescoSteps extends BaseTest {
 
     @And("the English flag and text is visible")
     public void theEnglishFlagAndTextIsVisible() {
-        WebElement englishFlagElement = driver.findElement(By.xpath("//button[@id='utility-header-language-switch-link']"));
-        if (englishFlagElement.isDisplayed()) {
-            System.out.println("The English flag is visible.");
-        }
+        homePage.checkEnglishFlag();
     }
 
     @When("I change the language to English")
     public void iChangeTheLanguageToEnglish() {
-            WebElement languageChangeElement = driver.findElement(By.xpath("//button[@id='utility-header-language-switch-link']"));
-                if (driver.getCurrentUrl().contains("/groceries/hu-HU/")) {
-                    languageChangeElement.click();
-                }
-
-    }
-
-    private void clickOnHungarianFlag() {
-        driver.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/nav[2]/div/div/div/ul/form")).click();
+        homePage.changeLanguageToEng();
     }
 
     @Then("the page language changed to English")
     public void thePageLanguageChangedToEnglish() {
+        homePage.checkLanguageChangedToEng();
 
     }
 
@@ -85,28 +70,19 @@ public class TescoSteps extends BaseTest {
 
     @And("the Hungarian flag and text is visible")
     public void theHungarianFlagAndTextIsVisible() {
-        WebElement englishFlagElement = driver.findElement(By.xpath("//button[@id='utility-header-language-switch-link']"));
-        if (englishFlagElement.isDisplayed()) {
-            System.out.println("The Hungarian flag is visible.");
-        }
+        homePage.checkHungarianFlag();
 
     }
 
     @When("I change the language to Hungarian")
     public void iChangeTheLanguageToHungarian() {
-            WebElement languageChangeElement = driver.findElement(By.xpath("//button[@id='utility-header-language-switch-link']"));
-            if (driver.getCurrentUrl().contains("/groceries/en-GB/")) {
-                languageChangeElement.click();
-            }
-        }
-
-    private void clickOnEnglishFlag() {
-        driver.findElement(By.xpath("/html/body/div[1]/div/div/div[2]/nav[2]/div/div/div/ul/form")).click();
+        homePage.changeLanguageToHun();
     }
 
 
     @Then("the page language changed to Hungarian")
     public void thePageLanguageChangedToHungarian() {
+        homePage.checkLanguageChangedToHun();
     }
 
 
